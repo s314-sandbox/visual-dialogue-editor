0a68dd5342c9a1ab7be2b2147170f31ede387613
s314 <s314.dev@gmail.com>
2018-02-01 20:06:17 +0300

    Added Response class

---
+++ response.h
@@ -0,0 +1,19 @@
+#ifndef RESPONSE_H
+#define RESPONSE_H
+
+#include <abstractentry.h>
+
+class Response : public AbstractEntry
+{
+private:
+    QString           p_text;      // Содержание ответа
+    AbstractEntry*    p_next;      // Ссылка на следующий элемент (кроме типа Response)
+public:
+    Response();
+    void             setText(const QString text);
+    QString          getText() const;
+    void             setNext(const AbstractEntry& next);
+    AbstractEntry*   getNext() const;
+};
+
+#endif // RESPONSE_H
